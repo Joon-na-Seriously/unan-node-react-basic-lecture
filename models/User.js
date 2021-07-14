f7e249f3c816e53ae9eeadef98c4ccbd4ff35519
unanchoi <unusualc@likelion.org>
2021-07-14 21:21:19 +0900

    node.js로 백엔드 구성

--- models/User.js
+++ models/User.js
@@ -44,8 +44,53 @@ userSchema.pre('save', function (next) {
                 next()
             })
         })
+    } else {
+        next()
+
     }
 })
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    //plainpassword 1234567 암호화된 비밀번호 $sdfsd232r5fsdfsfsfsdf 가 같은지 체크해야됨.
+
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(Cb) {
+    // jsonwebToken을 이용해서 token을 생성하기.
+    var user = this;
+
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = toKen
+    // ->
+    // 'secretToken' -> user._id
+   user.token = token 
+   user.save(function(err, user){
+
+    if(err) return cb(err)
+    cb(null, user)
+   })
+}
+
+userSchema.statics.findByToken = function(token , cb) {
+    var user = this;
+
+    // 토큰을 decode한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인.
+
+        user.findOne({"_id" : decoded, "token": token }, function(err, user){
+
+        if(err) return cb(err);
+        cb(null, user)
+    })
+})
+}
 
 const User = mongoose.model('User', userSchema)
 
