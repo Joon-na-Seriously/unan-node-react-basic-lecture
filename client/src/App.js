73220e165e531965c13b76693f2ac357fbad0b14
unanchoi <unusualc@likelion.org>
2021-07-18 01:51:08 +0900

    7.17 ~ 7.18 night

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth'
+
+
+export default function App() {
+  return (
+    <div>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">랜딩페이지</Link>
+            </li>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/register">레지스터</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          
+          <Route exact path="/"  component = {Auth(LandingPage,null )}> 
+            {/* <LandingPage />  를 Route 태그안에 넣어주거나 혹은 <Route component = {LandingPage} 둘다 가능 .*/}
+          </Route>
+
+          <Route path="/login" component = {Auth(LoginPage, false)} />
+          <Route path="/register" component = {Auth(RegisterPage, false)} /> 
+
+        </Switch>
+      </div>
+    </Router>
+    </div>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ const config = require('./config/key');
 const mongoose = require('mongoose');
 
 
-const { auth } = require('.//middleware/auth');
-const { User } = require('.//models/User');
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
+
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,6 +29,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요 새해복 많이 받으세여!~~~~~~~~')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("hellow world!")
+})
+
 app.post('/api/users/register', (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다.
@@ -41,7 +46,7 @@ app.post('/api/users/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   //요청된 이메일을 데이터베이스에 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, userInfo) => {
